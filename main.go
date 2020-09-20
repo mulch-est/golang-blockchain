12ff3508ff853349c5369e8dbac7b782036729f9
mulch <52766118+mulch-est@users.noreply.github.com>
2020-09-20 16:51:19 -0400

    revert previous

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 
 func main() {
 	defer os.Exit(0)
-	
-	//os.Setenv("NODE_ID", "7000") //Can uncomment for testing if you keep receiving <NODE_ID env is not set> errors
 
 	cmd := cli.CommandLine{}
 	cmd.Run()
